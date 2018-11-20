20cdab269f81c62fa88aa487a97cc78e6c2a8eb5
FernandoChavez <luis_fernando_chavez@hotmail.com>
2018-11-20 17:03:51 -0600

    change

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/techchaser/index.html'));
 });
 
+
 // Start the app by listening on the default
 // Heroku port
 //app.listen(process.env.PORT || 8080);
